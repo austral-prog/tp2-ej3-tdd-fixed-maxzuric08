f9d14a588698b1a4088cba7eeff3f924c8579cec
maxzuric08 <162721708+maxzuric08@users.noreply.github.com>
2024-08-23 09:47:11 -0400

    Update App.java

--- src/main/java/com/template/App.java
+++ src/main/java/com/template/App.java
@@ -4,11 +4,37 @@ import java.util.Collections;
 import java.util.Map;
 
 public class App {
-
-
     public static Map<String, Integer> deposit(Map<String, Integer> accounts, String account, int amount) {
-        return Collections.emptyMap();
+            if (amount<0) return accounts;
+            if (!accounts.containsKey(account)) return accounts;
+
+            accounts.put(account, accounts.get(account) + amount);
+
+        return accounts;
     }
+
+    public static Map<String, Integer> withdraw(Map<String, Integer> accounts, String account, int amount) {
+        if (!accounts.containsKey(account)) return accounts;
+        if (accounts.get(account) < amount || accounts.get(account) < 0) return accounts;
+        if (amount<0) return accounts;
+        accounts.put(account, accounts.get(account) - amount);
+
+        return accounts;
+
+    }
+
+    public static Map<String, Integer> transfer(Map<String, Integer> accounts,String account, String account_1, int amount ) {
+        if (!accounts.containsKey(account) || !accounts.containsKey(account_1)) return accounts;
+        if (accounts.get(account)<amount) return accounts;
+        if (amount<0) return accounts;
+        if (account==account_1) return accounts;
+
+        accounts.put(account, accounts.get(account) - amount);
+        accounts.put(account_1, accounts.get(account_1) + amount);
+        return accounts;
+
+    }
+
 
 
 }
